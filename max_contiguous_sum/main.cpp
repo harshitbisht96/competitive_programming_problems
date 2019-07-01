4de2d5694f7375dd65d91dd2cfa256e2e5d26a8f
harshitbisht96 <harshitbisht99@gmail.com>
2019-07-01 09:33:33 +0530

    Maximum contiguous sum solution

---
+++ max_contiguous_sum/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int max_so_far=INT_MIN,max_ending_here=0;
+    for(int i=0;i<n;i++)
+    {
+        max_ending_here+=a[i];
+        if(max_so_far<max_ending_here)
+        {
+            max_so_far=max_ending_here;
+        }
+        if(max_ending_here<0)
+        {
+            max_ending_here=0;
+        }
+    }
+    return 0;
+}
